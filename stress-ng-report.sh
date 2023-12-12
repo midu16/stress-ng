77e44a1f1b85b79ea4c9d16419c7db25924886af
Mihai Idu <midu@midu-thinkpadp1gen3.remote.csb>
2023-12-12 19:21:43 +0100

    adding the redirection to file

--- stress-ng-report.sh
+++ stress-ng-report.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
+# Output file
+output_file="stress-ng-report.log"
+
 # Function to print a separator line
 print_separator() {
-  echo "-------------------------"
+  echo "-------------------------" >> "$output_file"
 }
 
 # Loop through all nodes
 for index in $(oc get nodes -o 'jsonpath={.items[*].metadata.name}'); do
   # Print Node details
-  echo "Node: $index"
+  echo "Node: $index" >> "$output_file"
 
   # Print pods details in Stability Namespace on the Node and display top resources
-  oc get pods -A -o wide | awk -v zindex="$index" '/stability/ && $8 == zindex {print $0; system("oc adm top pod -n " $1 " " $2)}'
+  oc get pods -A -o wide | awk -v zindex="$index" '/stability/ && $8 == zindex {print $0; system("oc adm top pod -n " $1 " " $2)}' >> "$output_file"
   
   # Print the number of stress-ng Pods on the Node
   count=$(oc get pods -A -o wide | awk -v zindex="$index" '/stability/ && $8 == zindex {print $0}' | wc -l)
-  echo "Number of stress-ng Pods on Node $index: $count"
+  echo "Number of stress-ng Pods on Node $index: $count" >> "$output_file"
   
   # Print a separator line
   print_separator
 
   # Display node-wise resource consumption
-  oc adm top nodes $index
-done
+  oc adm top nodes $index >> "$output_file"
+done 
